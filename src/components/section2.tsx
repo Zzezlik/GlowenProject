a6ec40c7a9b3a4cd8b89176128eadac09f119d54
ath-00 <maxmandarin29@gmail.com@gmail.com>
2024-12-21 16:54:21 +0200

    Section 1 complete

--- src/components/section2.tsx
+++ src/components/section2.tsx
@@ -1,13 +1,31 @@
 const Section2 = () => {
 
     return (
-        <div className="bg-sec2Color text-white px-28 py-20">
+        <div className="bg-sec2Color flex flex-col text-white px-28 py-20">
 
-                <h1 className="text-5xl">
+            <div className="">
+                <h1 className="text-5xl ">
                     <b>ABOUT</b> <i>US</i>
                 </h1>
+            </div>
 
-                1
+            <div className="ml-72 mt-16">
+                <h1>
+                    GLOWEN – це не просто бренд світла. Це втілення мрії про світ, де кожен простір наповнений теплом і
+                    красою.
+                    Ми прагнемо дарувати людям радість і комфорт, створюючи атмосферу любові та єднання за допомогою
+                    інноваційних та якісних світлових рішень.
+
+                    Заснування GLOWEN стало результатом синергії талановитих людей, щиро віруючих у силу світла. Їхнє
+                    бачення полягає
+                    у перетворенні життя людей на краще за допомогою досконалого освітлення, яке підкреслює
+                    індивідуальність та дарує незабутні емоції.
+                </h1>
+
+                <button className="bg-white text-black py-2 px-20 rounded-3xl ">КОНТАКТИ</button>
+                <button className="bg-transparent border-2 text-white py-2 px-20 rounded-3xl">НАШ ІНСТАГРАМ </button>
+
+            </div>
 
         </div>
     )
